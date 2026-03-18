377a3abec7f914f8a870bc878cbee15809b94594
Harry Cost <i.poberejets@yandex.com>
2026-03-18 23:24:42 +0300

    level_1

--- tests/level_1/test_four_bank_parser.py
+++ tests/level_1/test_four_bank_parser.py
@@ -1,5 +1,46 @@
+from decimal import Decimal
 from functions.level_1.four_bank_parser import BankCard, SmsMessage, Expense, parse_ineco_expense
+from datetime import datetime
+import pytest
 
 
-def test_parse_ineco_expense():
-    pass
+def test__parse_ineco_expense__case1():
+    cards = [BankCard("2222", "Author"),
+             BankCard("3333", "Author"),
+             BankCard("1234", "Author")]
+    sms = SmsMessage("100 rub, 1234 01.01.26 21:14 Pyaterochka authcode 0000 ",
+                     "Author",
+                     datetime(2026, 1, 1))
+    result = Expense(amount=Decimal('100'),
+                     card=BankCard(last_digits='1234', owner='Author'),
+                     spent_in='Pyaterochka',
+                     spent_at=datetime(2026, 1, 1, 21, 14))
+
+    assert parse_ineco_expense(sms, cards) == result
+
+
+def test__parse_ineco_expense__no_card_in_cards():
+    cards = [BankCard("2222", "Author")]
+    sms = SmsMessage("100 rub, 1234 01.01.26 21:14 Pyaterochka authcode 0000 ",
+                     "Author",
+                     datetime(2026, 1, 1))
+    with pytest.raises(IndexError):
+        parse_ineco_expense(sms, cards)
+
+
+def test__parse_ineco_expense__wrong_time_in_massage():
+    cards = [BankCard("1234", "Author")]
+    sms = SmsMessage("100 rub, 1234 01.01.26 21-14 Pyaterochka authcode 0000 ",
+                     "Author",
+                     datetime(2026, 1, 1))
+    with pytest.raises(ValueError):
+        parse_ineco_expense(sms, cards)
+
+
+def test__parse_ineco_expense__wrong_massage():
+    cards = [BankCard("1234", "Author")]
+    sms = SmsMessage("100 rub, 1234 01.01.26 21:14 authcode 0000 test string",
+                     "Author",
+                     datetime(2026, 1, 1))
+    with pytest.raises(ValueError):
+        parse_ineco_expense(sms, cards)
--- tests/level_1/test_two_date_parser.py
+++ tests/level_1/test_two_date_parser.py
@@ -1,5 +1,26 @@
 from functions.level_1.two_date_parser import compose_datetime_from
+from datetime import datetime 
 
 
-def test_compose_datetime_from():
-    pass
+def test__compose_datetime_from__today():
+    date_str = ""
+    time_str = "15:16"
+    answer = datetime(datetime.today().year,
+                      datetime.today().month,
+                      datetime.today().day,
+                      15,
+                      16)
+
+    assert compose_datetime_from(date_str, time_str) == answer
+
+
+def test__compose_datetime_from__tomorrow():
+    date_str = "tomorrow"
+    time_str = "15:16"
+    answer = datetime(datetime.today().year,
+                      datetime.today().month,
+                      datetime.today().day+1,
+                      15,
+                      16)
+
+    assert compose_datetime_from(date_str, time_str) == answer
